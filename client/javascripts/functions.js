cf191291843921274834412169d9c3074d4ebfea
Lucas Hyatt <llhyatt98@gmail.com>
2020-04-13 23:21:35 -0700

    js functions

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -14,3 +14,85 @@ let containsDuplicates = s => {
   }
   return false;
 };
+
+
+// revWords1
+let revWords1 = sentence => {
+  let ret_str = '';
+  let word_arr = sentence.split(" ");
+  let temp = '';
+
+  for(let i = 0; i < word_arr.length; i++)
+  {
+    for(let j = 0; j < word_arr[i].length; j++)
+    { 
+      temp = word_arr[i][j] + temp;
+    }
+    if(i != word_arr.length-1){
+      temp += ' ';
+    }
+    ret_str += temp;
+    temp = '';
+  }
+  return ret_str
+}
+
+let test = revWords1("hello this is a test");
+console.log(test);
+
+
+// revword2
+let revWords2 = sentence => {
+  let ret_str = '';
+  let word_arr = sentence.split(" ");
+  let temp = '';
+
+  word_arr.forEach(elem => {
+    for(let j of elem)
+    { 
+      temp = j + temp;
+    }
+    if(elem != word_arr[word_arr.length-1]){
+      temp += ' ';
+    }
+
+    ret_str += temp;
+    temp = '';
+  });
+
+  return ret_str
+}
+
+let test2 = revWords2("hello this is a test");
+console.log(test2);
+
+
+
+// revword3
+let revWords3 = sentence => {
+  let ret_str = '';
+  let word_arr = sentence.split(" ");
+  let temp = '';
+
+  for(let i of word_arr)
+  {
+    for(let j of i)
+    { 
+      temp = j + temp;
+    }
+    if(i != word_arr[word_arr.length-1]){
+      temp += ' ';
+    }
+    ret_str += temp;
+    temp = '';
+  }
+  return ret_str
+}
+
+let test3 = revWords2("hello this is a test");
+console.log(test3);
+
+
+
+
+
